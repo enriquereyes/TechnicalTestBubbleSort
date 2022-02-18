f34dbc41f7e5eba3515e9fb0652bf211ce882235
Luis Enrique Reyes Serrano <lenrique.reyesse@gmail.com>
2022-02-18 01:35:10 -0600

    Bubble sort.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = { 2, 5, 6, 7, 3, 1, 4, 9, 8, 10 };
+Console.WriteLine("Unsorted: [{0}]", string.Join(", ", numbers));
+Console.WriteLine("Sorted: [{0}]", string.Join(", ", bubbleSort(numbers)));
+
+int[] bubbleSort(int[] numbers)
+{
+    int t = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        for (int j = 0; j < numbers.Length - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                t = numbers[j + 1];
+                numbers[j + 1] = numbers[j];
+                numbers[j] = t;
+            }
+        }
+    }
+    return numbers;
+}
